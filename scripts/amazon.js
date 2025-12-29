9ab54698743585c440d74c92186c535989bc2067
pankaj rathod <pankajrathod272004@gmail.com>
2025-12-29 19:57:13 +0530

    Added dropdown selection by quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) =>{
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -59,6 +59,7 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click', () => {
+
        const productId = button.dataset.productId;
        let matchingItem;
        cart.forEach((item) => {
@@ -67,12 +68,18 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
          }
        });
 
+      const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+      
+      const quantity = Number(quantitySelector.value); 
+      console.log(quantity);
+
+
        if (matchingItem) {
-          matchingItem.quantity += 1;
+          matchingItem.quantity += quantity;
        }else{
           cart.push({
           productId: productId,
-          quantity: 1
+          quantity: quantity
        });
        }
 
